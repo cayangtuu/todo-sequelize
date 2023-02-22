2576c1b69c770ce8ea0d774dc0b66ea8c584dc29
cayang <cayang@simenvi.com.tw>
2023-02-22 16:19:00 +0800

    feat: add auth

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const home = require('./modules/home')
 const users = require('./modules/users')
 const todos = require('./modules/todos')
 const { generalErrorHandler } = require('../middleware/error')
+const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
-router.use('/todos', todos)
-router.use('/', home)
+router.use('/todos', authenticator, todos)
+router.use('/', authenticator, home)
 router.use('/', generalErrorHandler)
 
 module.exports = router
